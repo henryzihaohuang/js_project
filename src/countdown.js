cd14996344e58d086d4e5d07d00648bb2b74753f
henryzihaohuang <atlas.bb.contact@gmail.com>
2021-02-10 11:37:48 -0600

    Fix timer

---
+++ src/countdown.js
@@ -0,0 +1,20 @@
+const countdown = () => {
+    window.onload = () => {
+
+        let minutes = 15;
+        let seconds = 59;
+
+        setInterval(() => {
+            seconds = (seconds % 60) < 10 ? "0" + seconds : seconds;
+            document.getElementById("countdown").innerHTML = minutes + " min : " + seconds + " sec before departure";
+            seconds--;
+
+            if (seconds === 0) {
+                minutes--;
+                seconds = 59;
+            }
+        }, 1000);
+    }
+}
+
+export default countdown;
